7cc97efd3a5919c3ad1f3840f1fb8e6a29677e46
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-10-04 00:22:29 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -3,9 +3,7 @@
 require_once("util-db.php");
 require_once("model-authors.php");
 
-
-
-$pageTitle= "Authors";
+$pageTitle = "Authors";
 include "view-header.php";
 $authors = selectAuthors();
 include "view-authors.php";
